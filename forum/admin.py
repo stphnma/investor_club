6c1ed20fa4b180ad6a02c384d2297c13bd8ea206
stephenma <stphnma@gmail.com>
2015-11-07 20:59:42 -0500

    Creating admin account

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import IdeaPost
+from .models import Comment
+
+admin.site.register(IdeaPost)
+admin.site.register(Comment)
+
 # Register your models here.
